e46c95dd45002d066e7c27a71df43bb54f1cb210
Markus Dreyer <markusdreyer@Markuss-MacBook-Pro.local>
2018-08-19 16:08:12 +0200

    clean: remove autobind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import autobind from 'auto-bind';
+import LazyLoad from 'react-image-lazy-load';
 
 import './App.css';
-import Navbar from './Navbar/Navbar';
-//import Plant from './Plant/Plant';
 
 class App extends Component {
 
@@ -12,13 +10,10 @@ class App extends Component {
     super(props)
 
     this.state = {
-      togglePhotoVisibility: false,
-      largeImageLoaded: false,
-      photo: null,
-      isLoading: false
+      photo: null
     }
 
-    autobind(this);
+    this.getNewRandomPhoto = this.getNewRandomPhoto.bind(this)
   }
 
   getNewRandomPhoto = () => {
@@ -36,15 +31,9 @@ class App extends Component {
       .then((res, rej) => {
         // success
         console.log('res', res.data)
-        const { photo } = this.state
-
-        // store old photo
-        if (photo) this.state.oldPhoto = photo
 
         this.setState({
-          photo: res.data,
-          tinyImageLoaded: false,
-          isLoading: true
+          photo: res.data
         })
       })
       .catch(err => {
@@ -57,23 +46,11 @@ class App extends Component {
       })
   }
 
-  handleLargeImageLoaded() {
-    this.setState({
-      largeImageLoaded: true,
-      isLoading: false
-    })
-  }
-
   render() {
-    const { photo, largeImageLoaded, isLoading, oldPhoto } = this.state
-
-    const showTinyImageClasses = `${largeImageLoaded ? 'tiny hide': 'tiny show'}`
-    const showLargeImageClasses = `${largeImageLoaded ? 'show': 'hide'}`
-    const photoWrapperClasses = `${isLoading ? 'photo-wrapper white-overlay' : 'photo-wrapper'}`
+    const { photo } = this.state
 
     return (
       <div className="App">
-      
       <button
           className='moistureButton'
           onClick={() => this.getNewRandomPhoto()}>
@@ -81,18 +58,15 @@ class App extends Component {
         </button>
 
           {this.state.photo && (
-            <div className={photoWrapperClasses}>
-              <img
-                src={photo.urls.thumb}
-                alt="biiig random photo from unsplash"
-                className={showTinyImageClasses}
-                onLoad={this.handleTinyImageLoaded}></img>
-              <img
-                src={isLoading && oldPhoto ? oldPhoto.urls.full : photo.urls.full}
-                alt="smaaaall random photo from unsplash"
-                onLoad={this.handleLargeImageLoaded}
-                className={showLargeImageClasses}></img>
-            </div>
+            <LazyLoad
+              height={1000}
+              loaderImage
+              originalSrc={photo.urls.full}
+              imageProps={{
+                src: photo.urls.thumb,
+                alt: "DR_MVMQ20Feb2015ouellet1024.jpg",
+                ref: "image"
+              }} />
             )
           }
       </div>
